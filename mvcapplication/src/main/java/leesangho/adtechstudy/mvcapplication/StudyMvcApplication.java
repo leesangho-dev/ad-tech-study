d956240bb549056dab25b21bf62ebd6c67868bcf
이상호 <emart@emartui-MacBookPro.local>
2023-11-06 10:24:18 +0900

    feat: 초기 스터디 프로젝트 구성 및 환경 구성

---
+++ mvcapplication/src/main/java/leesangho/adtechstudy/mvcapplication/StudyMvcApplication.java
@@ -0,0 +1,12 @@
+package leesangho.adtechstudy.mvcapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyMvcApplication.class, args);
+    }
+}
