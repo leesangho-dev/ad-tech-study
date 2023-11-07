6b6df95812e3f8c721580d58f73289f2d4d349fa
이상호 <emart@emartui-MacBookPro.local>
2023-11-07 15:09:41 +0900

    feat: webflux application 추가

---
+++ webflux-application/src/main/java/leesangho/adtechstudy/webfluxapplication/StudyWebfluxApplication.java
@@ -0,0 +1,12 @@
+package leesangho.adtechstudy.webfluxapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "leesangho.adtechstudy")
+public class StudyWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyWebfluxApplication.class, args);
+    }
+}
